46bd4adc970185440fa417bc0fc46a744dba3484
Jade Page <jadepage@gmail.com>
2022-02-13 15:09:13 +0200

    completed the sidebar profile section

---
+++ src/Sidebar.js
@@ -0,0 +1,47 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1644694554482-21ce08832b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Jade Page</h2>
+        <h4>jadepage@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,564</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
